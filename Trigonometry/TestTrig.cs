f9f676e99a9dd7fe7f5d0e78871211ee09a977de
randall-pt <64786966+randall-pt@users.noreply.github.com>
2021-05-28 13:50:32 +0100

    Initial commit

---
+++ Trigonometry/TestTrig.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trigonometry;
+
+namespace Trigonometry
+{
+    class TestTrig
+    {
+        public static void Main(string[] args)
+        {
+            string fname = @"Assets\triangle_test_input.csv";
+            fname = @"C:\Users\pault\source\repos\Trigonometry\Trigonometry\Assets\triangle_test_input.csv";
+            Console.WriteLine("I am doing stuff thank you!");
+            List<Triangle> tris = GetTestData(fname);
+            foreach (Triangle t in tris)
+            {
+                Console.WriteLine($"{t.SideA}, {t.SideB}, {t.SideC}, {t.AngA}, {t.AngB}, {t.AngC}");
+            }
+        }
+
+        private static List<Triangle> GetTestData(string fname)
+        {
+            List<Triangle> lst = new List<Triangle>();
+
+            StreamReader sr = new StreamReader(fname);
+
+
+            while (!sr.EndOfStream)
+            {
+                string rline = sr.ReadLine();
+                string[] lineData = new string[6];
+                string singleValue = "";
+                int i = 0;
+
+                // Seperates the data out from the readLine of the csv file
+                for(int j = 0; j < rline.Length; j++)
+                {
+
+                    if (rline[j] == ',')
+                    {
+                        lineData[i] = singleValue;
+                        singleValue = "";
+                        i++;
+                    }
+                    else
+                    {
+                        singleValue += rline[j];
+                        // if the pointer is at the end of the line will add the value to the line Data array
+                        if (j == rline.Length - 1)
+                        {
+                            lineData[i] = singleValue;
+                        }
+                    }
+
+
+                }
+                // catches any erronous inputs into the csv file, and prints a warning
+                try
+                {
+                    lst.Add(new Triangle(lineData[0], lineData[1], lineData[2], lineData[3], lineData[4], lineData[5]));
+                }
+                catch
+                {
+                    Console.WriteLine($"The following input was not valid:\n {lineData[0]}, {lineData[1]}, {lineData[2]}, {lineData[3]}, {lineData[4]}, {lineData[5]}\n---------------------");
+                }
+            }
+
+            return lst;
+        }
+    }
+}
---
+++ Trigonometry/Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Trigonometry
+{
+    public class Triangle
+    {
+        public double SideA
+        { get; private set; }
+        public double SideB
+        { get; private set; }
+        public double SideC
+        { get; private set; }
+        public double AngA
+        { get; private set; }
+        public double AngB
+        { get; private set; }
+        public double AngC
+        { get; private set; }
+
+        public Triangle(string a = "-1", string b = "-1", string c = "-1", string angA = "-1", string angB = "-1", string angC = "-1")
+        {
+            this.SideA = Convert.ToDouble(a);
+            this.SideB = Convert.ToDouble(b);
+            this.SideC = Convert.ToDouble(c);
+            this.AngA = Convert.ToDouble(angA);
+            this.AngB = Convert.ToDouble(angB);
+            this.AngC = Convert.ToDouble(angC);
+        }
+
+        public Triangle(double a = -1, double b = -1, double c = -1, double angA = -1, double angB = -1, double angC = -1)
+        {
+            this.SideA = a;
+            this.SideB = b;
+            this.SideC = c;
+            this.AngA = angA;
+            this.AngB = angB;
+            this.AngC = angC;
+        }
+
+    }
+}
